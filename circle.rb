7c9c3a5a0837e852c922c491aadead6d0196d4fb
Matthew James <matthew.james@legalaid.gsi.gov.uk>
2018-10-11 12:55:26 +0100

    Add changes to circle file

--- circle.rb
+++ circle.rb
@@ -1,14 +1,22 @@
 class Circle
-  def initialize (radius)
+  def initialize (radius, colour)
     @radius = radius
     @PI = Math::PI
+    @colour = colour
   end
 
   def circle_area
-    area = @PI * @radius * @radius
+    area = @PI * @radius**2
   end
+
+  def description
+    circle_text = "A circle with radius of " + @radius.to_s + " has an area of "\
+    + sprintf("%#.2f",circle_area).to_s + "cm2 and its colour is " + @colour
+  end
+
+  attr_accessor :radius, :colour
 
 end
 
-big_circle = Circle.new(66.6)
-puts big_circle.circle_area
+big_circle = Circle.new(66.6, "blue")
+puts big_circle.description
